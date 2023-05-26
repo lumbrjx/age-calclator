e239298c4800f628aeb76dbb092feed4480f2cd9
CLog <88345999+CLOG9@users.noreply.github.com>
2023-05-26 11:35:35 +0200

    setting up the fonts and colors and folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1 className="font-Poppins text-myPurple">hello</h1>
+    </>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Poppins: ["Poppins"],
+      },
+      colors: {
+        myPurple: "hsl(259, 100%, 65%)",
+        mylightred: "hsl(0, 100%, 67%)",
+        myoff_white: "hsl(0, 0%, 94%)",
+        mylight_grey: "hsl(0, 0%, 86%)",
+        mysmokey_grey: "hsl(0, 1%, 44%)",
+        myoff_black: "hsl(0, 0%, 8%)",
+      },
+    },
   },
   plugins: [],
   mode: "jit",
